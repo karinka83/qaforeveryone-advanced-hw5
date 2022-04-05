5cdff4a83903d0be5a498653141b2601b8ffc915
karinka83 <karina.kulakova@gmail.com>
2022-04-05 20:30:54 +0300

    sozdala w q

---
+++ src/com/karinka83/Task2.java
@@ -0,0 +1,15 @@
+package com.karinka83;
+
+public class Task2 {
+    public static void main(String[] args) {
+        int n = 10; //strings
+
+        for(int i1=n-1; i1>0;i1--)
+            System.out.print(i1);
+
+            for(int i1 =0; i1 <n;i1++){
+                System.out.print(i1);
+                System.out.println();
+        }
+    }
+}
